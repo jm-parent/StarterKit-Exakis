a7989b1775c346ee01f9e4f57d7d07aa7ca82785
jm-parent <39386863+jm-parent@users.noreply.github.com>
2021-11-15 13:11:35 +0100

    udpate clean

--- StarterKit/StarterKit/Views/Popups/MyPopupView.xaml.cs
+++ StarterKit/StarterKit/Views/Popups/MyPopupView.xaml.cs
@@ -11,42 +11,21 @@ namespace StarterKit.Views.Popups
 		public MyPopupView()
 		{
 			InitializeComponent();
-			Opened += MyPopupView_OpenedAsync;
+			Opened += MyPopupView_Opened;
 		}
 
-		private void MyPopupView_OpenedAsync(object sender, PopupOpenedEventArgs e)
+		private void MyPopupView_Opened(object sender, PopupOpenedEventArgs e)
 		{
-			Task.Run(() =>
-			{
-				Animation b = new Animation();
-				b.Add(0, 1, new Animation(v => this.Opacity = v, 0, 1));
-				b.Commit(
-					 owner: ThePopup,
-					 name: "show",
-					 length: 1200);
-			});
-			
+			Animation b = new Animation();
+			b.Add(0, 1, new Animation(v => this.Opacity = v, 0, 1));
+			b.Commit(
+				 owner: ThePopup,
+				 name: "show",
+				 length: 1200);
 		}
-
-	
-		//private void MyPopupView_SizeChanged(object sender, EventArgs e)
-		//{
-
-		//}
-
-		//private void ThePopup_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		//{
-		//	if(e.PropertyName == nameof(IsVisible))
-		//	{
-		//		ThePopup.FadeTo(0, 1);
-		//		ThePopup.FadeTo(1, 3000);
-		//	}
-		//}
-
 		void Button_Clicked(System.Object sender, System.EventArgs e)
 		{
 			Dismiss(null);
 		}
-	
 	}
 }
