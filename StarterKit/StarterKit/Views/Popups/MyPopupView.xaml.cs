143c6e9ecbb980b26d7c22c736e84264ad7761f0
jm-parent <39386863+jm-parent@users.noreply.github.com>
2021-11-18 09:20:15 +0100

    Revert "some tests"
    This reverts commit 127d6aad658d26a1e668444637a5fa497e4e9867.

--- StarterKit/StarterKit/Views/Popups/MyPopupView.xaml.cs
+++ StarterKit/StarterKit/Views/Popups/MyPopupView.xaml.cs
@@ -16,25 +16,12 @@ namespace StarterKit.Views.Popups
 
 		private void MyPopupView_Opened(object sender, PopupOpenedEventArgs e)
 		{
-
 			Animation b = new Animation();
-			b.Add(0, 0.7, new Animation(v => this.Opacity = v, 0, 1));
-
-			//Popup scale animation
-			b.Add(0, 0.5, new Animation(v => TheContent.Margin = v, 150,0));
-
-			//From x:Name="Button" FontSize="1" 
-			b.Add(0.5, 1, new Animation(v => TheLabel.FontSize = v, 1, 16));
-			b.Add(0.5, 1, new Animation(v => TheButton.FontSize = v, 1, 16));
-
-			b.Add(0, 0.5, new Animation(v => TheButton.HeightRequest = v, 0, 0));
-			b.Add(0.5, 1, new Animation(v => TheButton.HeightRequest = v, 0, 50));
-
-
+			b.Add(0, 1, new Animation(v => this.Opacity = v, 0, 1));
 			b.Commit(
 				 owner: ThePopup,
 				 name: "show",
-				 length: 1000);
+				 length: 1200);
 		}
 		void Button_Clicked(System.Object sender, System.EventArgs e)
 		{
